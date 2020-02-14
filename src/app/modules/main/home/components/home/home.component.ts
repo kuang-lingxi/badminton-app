50cb43cb6a26d3acdef27926b9b8e9fbcbb54a32
kuanglingxi <1148273143@qq.com>
2020-02-14 15:27:20 +0800

    ADD: improve home page

--- src/app/modules/main/home/components/home/home.component.ts
+++ src/app/modules/main/home/components/home/home.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  state = {
+    data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+    imgHeight: '184px'
+  };
+
+  activeKey = [1];
+
+  accordions: Array<any> = [
+    { title: '3v3趣味赛', introduce: '3v3趣味赛介绍', beginTime: 1580808088000, endTime: 1580808088000},
+    { title: '3v3趣味赛', introduce: '3v3趣味赛介绍', beginTime: 1580808088000, endTime: 1580808088000},
+    { title: '3v3趣味赛', introduce: '3v3趣味赛介绍', beginTime: 1580808088000, endTime: 1580808088000}
+  ];
+
+  noticeList = [
+    {id: 0, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 1},
+    {id: 1, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 1},
+    {id: 2, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 1},
+    {id: 3, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 0},
+    {id: 4, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 0},
+    {id: 5, title: '第一条公告', content: '这是第一条公告的内容', time: 1580808088000, top: 0},
+  ]
+
+  modal = {0: false, 1: false, 2: false, 3: false,4: false, 5: false}
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onClose(id) {
+    this.modal[id] = false;
+  }
+
+  onChange(event) {
+    console.log(event);
+  }
+
+  
 
 }
--- src/app/modules/main/home/home.module.ts
+++ src/app/modules/main/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelfCommonModule } from '../../common/common.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
@@ -9,7 +10,8 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SelfCommonModule
   ]
 })
 export class HomeModule { }
